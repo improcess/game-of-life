9207d6d82dcf237e68931af22011b82edafe1d05
Sam Aaron <samaaron@gmail.com>
2012-03-24 10:21:17 +0000

    get things working - much polish still to be done though

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject game-of-life "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [overtone "0.7.0-SNAPSHOT"]
+                 [polynome "0.3-SNAPSHOT"]])
--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,17 +1,25 @@
 (ns game-of-life.core
-  (:use
-   overtone.live)
+  (:use [overtone.live]
+        [overtone.device.grid])
   (:require [polynome.core :as poly]
-            [overtone-contrib.core :as contrib]))
+            [overtone.at-at :as at]))
 
-(def m (poly/init "/dev/tty.usbserial-m64-0790"))
+;;(def m (poly/init "/dev/tty.usbserial-m64-0790"))
+(def m (poly/init "/dev/tty.usbserial-m256-203"))
+(comment
+  (poly/on-press m ::toggle (fn [x y s] (poly/toggle-led m x y)))
+  (poly/all m)
+  (poly/disconnect m)
+  (time (draw-state)))
 
 (def state (atom (zipmap (poly/coords m) (repeat 0))))
 
 (defn draw-state
-  ([] (apply poly/frame m (poly/map->frame m @state)))
-  ([time]
-     (apply poly/frame-at m time (poly/map->frame m @state))))
+  []
+  (dorun
+   (map (fn [[[x y] led-val]]
+          (poly/led m x y led-val))
+        @state)))
 
 (defn num-alive-neighbours
   [coords world]
@@ -48,20 +56,53 @@
   [state]
   (into {} (map #(vec (list % (if (lives? % state) 1 0))) (keys state))))
 
+
 (defn update-state
   []
   (swap! state evolve))
 
-(def metro (metronome 880))
+(def metro (metronome 250))
 
 (defn go [beat]
   (let [time (metro beat)]
-    (draw-state-at time)
+    (at/at time #(draw-state))
+;;    (println "hi")
     (update-state)
-    (contrib/apply-before #'go (metro (inc beat)) (inc beat))))
+    (apply-at (metro (inc beat)) #'go [(inc beat)])))
 
 (go (metro))
+(poly/on-press m ::start-life (fn [x y s]
+                                (poly/led-on m x y)
+                                (swap! state assoc [x y] 1)))
 
-(poly/on-release m (fn [x y] (swap! state assoc [x y] 1)))
+(comment
+  (get @state [4 0])
+  (poly/on-press m ::beep (fn [x y s] (demo 0.2 (sin-osc))))
+  (swap! state assoc [1 1] 1 [1 2] 1)
+  (poly/on-press m ::debug (fn [x y s] (println "Pressed: " x y)))
+  (poly/on-press m ::light (fn [x y s] (poly/led-on m x y)))
+  (poly/on-release m ::dark (fn [x y s] (poly/led-off m x y)))
 
+  (poly/led-off m 1 1)
+  (poly/led-on m 1 1)
 
+  (poly/disconnect m)
+
+  (poly/remove-all-callbacks m)
+  (stop))
+(count (:queue m))
+
+(dotimes [x 100]
+
+  (do
+    (apply poly/single-frame m  0 f2 )
+    (apply poly/single-frame m  1 f2 )
+    (apply poly/single-frame m  2 f2 )
+    (apply poly/single-frame m  3 f2 ))
+  (Thread/sleep 200)
+  (do
+    (apply poly/single-frame m  0 f )
+    (apply poly/single-frame m  1 f )
+    (apply poly/single-frame m  2 f )
+    (apply poly/single-frame m  3 f ))
+  (Thread/sleep 200))
